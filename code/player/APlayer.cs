db983ffdf6ece7155bfc6f855aa62656bc6868ce
Andorek <andorekgaming@gmail.com>
2023-01-10 17:03:13 +1100

    Changed some things on the player

--- code/player/APlayer.cs
+++ code/player/APlayer.cs
@@ -4,24 +4,30 @@ using ABase.Utilities;
 
 namespace ABase.Player;
 
-public partial class APlayer
-{
-	public APawn[] pawns;
+public partial class APlayer {
+
 	public AGlobalConfig config;
 
 	public APlayer() {
-		ADataUtil.GetData(out config);
-		pawns = new APawn[config.DefaultMaxCharacters];
+		// No id provided. Make a bot or something?
+	}
 
-		Dictionary<long, ACharacterInfo> playerInfo = new();
+	// charIndex allows you to choose which character the player should load up as. Useful if they have multiple characters from when AllowMultipleCharacters was true, but then changed to false.
+	public APlayer(IClient cl, int charIndex = 0) {
+		ADataUtil.GetData(out config);
+		var pawn = new APawn();
+
+		List<ACharacterInfo> playerInfo = new();
 
 		if (ADataUtil.DataExists("PlayerInfo")) { // The player has joined before.
-			ADataUtil.GetData("PlayerInfo", out playerInfo);
+			ADataUtil.GetData("PlayerInfo", cl.SteamId, out playerInfo);
+			if (!config.AllowMultipleCharacters) {
+				pawn.CharacterInfo = playerInfo[charIndex];
+			}
 		}
 		else { // The player has not joined before.
 
 		}
-
-		
 	}
+
 }
